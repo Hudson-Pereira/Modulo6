9fb5853089044ca94bb0a08b7e57d8511425bfb8
Hudson-Pereira <hudson.o.pereira@gmail.com>
2022-03-29 21:09:21 -0300

    aula 3 full

--- index.js
+++ index.js
@@ -1,49 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const port = 3000;
 const app = express();
-const cors = require('cors');
+const route = require('./src/routes/paletas.route');
 
 app.use(cors());
 app.use(express.json());
 
-const paletas = [
-  {
-    id: 1,
-    sabor: 'Primeiro',
-    descricao: 'Primeiro sabor',
-    foto: 'assets/images/acai-com-leite-condensado.png',
-    preco: 10.0,
-  },
-  {
-    id: 2,
-    sabor: 'Segundo',
-    descricao: 'Segundo sabor',
-    foto: 'assets/images/banana-com-nutella.png',
-    preco: 10.0,
-  },
-  {
-    id: 3,
-    sabor: 'Chocolate Belga',
-    descricao:
-      'Quam vulputate dignissim suspendisse in est ante in nibh mauris.',
-    foto: 'assets/images/chocolate-belga.png',
-    preco: 7.0,
-  },
-];
-
-app.get('/', function (req, res) {
-  res.send('Hello World');
-});
-
-app.get('/paletas/find-paletas', (req, res) => {
-  res.send(paletas);
-});
-
-app.get('/paletas/find-paleta/:id', (req, res) => {
-  const idParam = req.params.id;
-  const chosenPaleta = paletas.find((paleta) => paleta.id == idParam);
-  res.send(chosenPaleta);
-});
+app.use('/paletas', route);
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
